e1af4b3541c212d6ddecc2dcda4a9e28cc0f2c36
Micah Halter <micah@mehalter.com>
2023-05-11 07:40:03 -0400

    fix(autocmds): only get buffer infromation on BufEnter event

--- lua/astronvim/autocmds.lua
+++ lua/astronvim/autocmds.lua
@@ -143,22 +143,23 @@ if is_available "alpha-nvim" then
   autocmd("User", {
     desc = "Disable status and tablines for alpha",
     group = group_name,
-    pattern = "AlphaReady",
-    callback = function()
-      local prev_showtabline = vim.opt.showtabline
-      local prev_status = vim.opt.laststatus
-      vim.opt.laststatus = 0
-      vim.opt.showtabline = 0
-      vim.opt_local.winbar = nil
-      autocmd("BufUnload", {
-        desc = "Reenable status and tablines for alpha",
-        group = group_name,
-        pattern = "<buffer>",
-        callback = function()
-          vim.opt.laststatus = prev_status
-          vim.opt.showtabline = prev_showtabline
-        end,
-      })
+    callback = function(event)
+      if
+        (
+          (event.event == "User" and event.file == "AlphaReady")
+          or (event.event == "BufEnter" and vim.api.nvim_get_option_value("filetype", { buf = event.buf }) == "alpha")
+        ) and not vim.g.before_alpha
+      then
+        vim.g.before_alpha = { showtabline = vim.opt.showtabline:get(), laststatus = vim.opt.laststatus:get() }
+        vim.opt.showtabline, vim.opt.laststatus = 0, 0
+      elseif
+        vim.g.before_alpha
+        and event.event == "BufEnter"
+        and vim.api.nvim_get_option_value("buftype", { buf = event.buf }) ~= "nofile"
+      then
+        vim.opt.laststatus, vim.opt.showtabline = vim.g.before_alpha.laststatus, vim.g.before_alpha.showtabline
+        vim.g.before_alpha = nil
+      end
     end,
   })
   autocmd("VimEnter", {
